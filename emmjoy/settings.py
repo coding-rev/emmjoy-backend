d357a3b2d30d8f1a38d2e746dff6aa6d4b099efa
codingrev <emmowu10@gmail.com>
2021-04-11 23:20:46 -0700

    Upgrade

--- emmjoy/settings.py
+++ emmjoy/settings.py
@@ -9,12 +9,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = '*l^ce$ygdh=4ksn@@ll)w4d3^@cf!@9mqzfggbv+=h-330*s_='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['emmjoy-backend.herokuapp.com']
 
 
 # Application definition
@@ -28,6 +28,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'website',
+    'crispy_forms',
     'rest_framework',
     'rest_framework.authtoken',
 ]
