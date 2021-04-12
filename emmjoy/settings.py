22d63bebc0474f7186954619f09dfa773b05cc95
codingrev <emmowu10@gmail.com>
2021-04-11 23:51:41 -0700

    Upgraded

--- emmjoy/settings.py
+++ emmjoy/settings.py
@@ -12,9 +12,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '*l^ce$ygdh=4ksn@@ll)w4d3^@cf!@9mqzfggbv+=h-330*s_='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['emmjoy-backend.herokuapp.com', '*']
 
 
 # Application definition
