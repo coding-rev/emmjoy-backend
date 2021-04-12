08de6a3d86d5082ee79c4109cb67027fb80ff4f2
codingrev <emmowu10@gmail.com>
2021-04-11 22:53:28 -0700

    upgrade

--- emmjoy/settings.py
+++ emmjoy/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '*l^ce$ygdh=4ksn@@ll)w4d3^@cf!@9mqzfggbv+=h-330*s_='
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -28,7 +28,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'website',
-    'crispy_forms',
     'rest_framework',
     'rest_framework.authtoken',
 ]
